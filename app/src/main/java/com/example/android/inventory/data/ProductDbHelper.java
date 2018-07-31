191bfd604d12a1598a10b92bd70ca06b294179e6
javierfurus <javierfurus@gmail.com>
2018-07-31 09:16:24 -0400

    Removed unnecessary leftover  DROP TABLE from testing

--- app/src/main/java/com/example/android/inventory/data/ProductDbHelper.java
+++ app/src/main/java/com/example/android/inventory/data/ProductDbHelper.java
@@ -31,13 +31,11 @@ public class ProductDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
+
     }
 
     @Override
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
+
     }
 }
